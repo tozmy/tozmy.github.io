3cd55e0135fed784a071370957cabffed87f7474
tolrs <leizhixuan1999@163.com>
2019-12-25 02:39:16 +0800

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+   Pure css Santa Caul by
+      Alireza Sheikholmolouki
+      
+   Share this pen if you like it :)
+   
+   and this is my twitter account if you want to follow me:
+   
+      @alireza29675
+*/
