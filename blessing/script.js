ae772c1e38e3cc08c609c061f82fa069e7b18744
tozmy <59152859+tozmy@users.noreply.github.com>
2021-07-16 16:43:39 +0800

    🌕V1.2.0

--- blessing/script.js
+++ blessing/script.js
@@ -16,6 +16,7 @@ new Vue({
           source: "https://db.songqi.online/go-to-a-place-where-the-sun-shines.mp3",
           url: "https://music.163.com/song?id=1465230223&userid=380194786",
           blessing: "生日快乐生日快乐生日快乐",
+          hide: "",
           author: "小树",
           favorited: false
         },
@@ -26,6 +27,7 @@ new Vue({
           source: "https://db.songqi.online/everglow.mp3",
           url: "https://music.163.com/song?id=37239018&userid=380194786",
           blessing: "月月月月月月，我心目中美好存在的月月，多么幸运彼此遇见！不知不觉认识3年了，如你所说时间就像是坐上了火箭，分别的日子竟也已满1年，我一直觉得自己性格无趣遇事逃避久处生厌，但月月你知道吗，你的出现治愈了我，you give me this feeling this everglow！<br><br>我记得疫情在家你发来的零点生日祝福，记得你陪我改文章熬夜，记得你发的微博，记得你的礼物卡片，记得做工程前你对我的开导，也记得我们一起消食逛操场，一起吃火锅，一起玩游戏，一起唱歌，一起喝酒，一起失眠一整夜，一起在青衣江边聊天一整夜，你带给我的温暖我都清清楚楚的记得，也一直清清楚楚的感受着💞！你的柔软和共情总是能在我脆弱时第一时间安慰到我，在我低谷时给与我自信，在我动摇时让我内心坚定。<br><br>月月，不瞒你说，你是我心目中的情感咨询师和心理导师，真的🆘！虽然提供不了到位的心理咨询，但我愿意做你的倾听者，做你不开心时的树洞🌲🕳️！月月，你说的，我值得，我想说，你更值得，所以想和月月做能够一起度过漫长岁月的朋友👭！<br><br>少女与爱永不老去，月月生日快乐，祝你开心，不止今天",
+          hide: "",
           author: "婷婷",
           favorited: false
         },
@@ -35,7 +37,8 @@ new Vue({
           cover: "https://db.songqi.online/zmy.jpg",
           source: "https://db.songqi.online/breath-of-light.mp3",
           url: "https://c.y.qq.com/base/fcgi-bin/u?__=oQk6qn45",
-          blessing: "祝月月生日快乐<br><br>在未来的日子里振翅高飞<br><br>（PS：益文特别要求替换封面照片！）",
+          blessing: "祝月月生日快乐<br><br>在未来的日子里振翅高飞",
+          hide: "<br>PS：益文要求替换封面照片！",
           author: "文文",
           favorited: false
         },
@@ -46,6 +49,7 @@ new Vue({
           source: "https://db.songqi.online/crush.mp3",
           url: "https://music.163.com/song?id=516358165&userid=380194786",
           blessing: "生日快乐！",
+          hide: "这是隐藏文本",
           author: "弟弟",
           favorited: false
         },
@@ -56,6 +60,7 @@ new Vue({
           source: "https://db.songqi.online/starlight.mp3",
           url: "https://music.163.com/song?id=19292813&userid=380194786",
           blessing: "我见过一张脸蛋，既不引人注目的迷人，又迷人的引人注目。<br><br>是牵着我的手、是陪我一起睡不着、是欢快干饭、是一起学习、是有过很多个一起时光的人的脸蛋。<br><br>她带着尴尬的我融入了她的集体，拉着唱歌跑调的我一起唱歌，她总觉得我很好使我膨胀。她是我的小盆友，也是我的大朋友。<br><br>这么好的月月一定要永远开心，梦想成真，让我把你变成孩子吧！",
+          hide: "",
           author: "倩倩",
           favorited: false
         },
@@ -66,6 +71,7 @@ new Vue({
           source: "https://db.songqi.online/you-were-a-teenager.mp3",
           url: "https://music.163.com/song?id=1368371706&userid=380194786",
           blessing: "咱们都已经三年的饭友了，一起约了那么多回饭，还是第一次给你过生日，这是一次有特殊意义的约饭，真不错。今后的日子里我们还要继续约饭呐，毕竟还有那么多美食没吃遍呢。<br><br>这么好的月月一定要永远开心，梦想成真，让我把你变成孩子吧！",
+          hide: "",
           author: "小宋",
           favorited: false
         },
@@ -76,6 +82,7 @@ new Vue({
           source: "https://db.songqi.online/cheers.mp3",
           url: "http://www.kuwo.cn/play_detail/78488053",
           blessing: "往事不回头<br><br>未来不将就<br><br>希望你眼里总有光<br><br>活成自己喜欢的样子<br><br>生日快乐呀",
+          hide: "",
           author: "洋小汶",
           favorited: false
         },
@@ -86,7 +93,19 @@ new Vue({
           source: "https://db.songqi.online/i-sing-i-swim.mp3",
           url: "https://music.163.com/#/song?id=4281819&userid=541543776",
           blessing: "️🈷️🈷️生日快乐！<br><br>距离上次告别已经过去37天了！还记得第一次见面在老区恰的烤肉，此后更成为好朋友。每次出去玩总是特别开心，现在想想唯一遗憾的是少拍了很多照片。不出意外的话今年的毕业旅行应该出意外泡汤了。不过明年你毕业的时候我们四人的毕业旅行一定要安排上！<br><br>愿你永远是一个漂亮的少女！永远是一个开心的人儿！<br><br>🎉🎉生日快乐！",
+          hide: "",
           author: "老板",
+          favorited: false
+        },
+        {
+          name: "逝去的歌",
+          artist: "旅行团",
+          cover: "https://db.songqi.online/dead-song.jpg",
+          source: "https://db.songqi.online/dead-song.mp3",
+          url: "https://music.163.com/#/song?id=36199725",
+          blessing: "温柔美丽的月月～生日快乐🎂🎂<br><br>出生在夏天的你一定有一颗比夏天热烈的心<br><br>愿你能遇到值得心动的人<br><br>更愿你能一直保持内心的柔软<br><br>与这个世界拥抱和解<br><br>继续走下去<br><br>带着希望和更多的爱<br><br>爱自己爱这个世界<br><br>直到世间所有美好都与你相遇:)",
+          hide: "",
+          author: "苏苏",
           favorited: false
         }
       ],
@@ -188,6 +207,11 @@ new Vue({
       this.tracks[this.currentTrackIndex].favorited = !this.tracks[
         this.currentTrackIndex
       ].favorited;
+      // if(!this.tracks[this.currentTrackIndex].favorited){
+      //   this.tracks[this.currentTrackIndex].hide="隐藏";
+      //  }else{
+      //   this.tracks[this.currentTrackIndex].hide="显示";
+      //  }
     }
   },
   created() {
